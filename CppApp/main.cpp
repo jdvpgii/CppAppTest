0e66d40e414a1cdc09643e49a3b44b0ba210a7f9
jdvpgii <gii.jdvp@gmail.com>
2024-04-15 04:27:06 +0500

    start

--- CppApp/main.cpp
+++ CppApp/main.cpp
@@ -7,8 +7,14 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+int main()
+{
+    std::cout << "Hello, Islam!\n";
     return 0;
 }
