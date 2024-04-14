7dd6bb967c8ef5cd89dfb74fed1c13b8f2dca888
jdvpgii <gii.jdvp@gmail.com>
2024-04-15 03:43:13 +0500

    Initial Commit

---
+++ CppApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppApp
+//
+//  Created by Islam Gapisov on 15.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
